ced49bd02d7d1cb82b8091bb386b36158a5e7747
MarioArmstrong <arms.mario@icloud.com>
2022-11-12 22:17:52 -0800

    started looking at resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -76,18 +76,8 @@ const resolvers = {
   },
 
   //DELETE Book
-  deleteBook: async ({ user, params }, res) => {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
-      { new: true }
-    );
-    if (!updatedUser) {
-      return res
-        .status(404)
-        .json({ message: "Couldn't find user with this id!" });
-    }
-    return res.json(updatedUser);
+  deleteBook: async (parent, { bookId }) => {
+    return Book.findOneAndDelete({ _id: bookId });
   },
 };
 
